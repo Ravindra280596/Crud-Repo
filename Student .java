26e2a3e96c6278d0d0f1a1fb38848834e52ec85d
Ravindra280596 <85027773+Ravindra280596@users.noreply.github.com>
2021-06-09 19:32:06 +0530

    Update Student .java

--- Student .java
+++ Student .java
@@ -2,6 +2,7 @@ Class Student{
 int id;
 String name;
 public static void main(string args[]){
-System.out.println("Hello World")
+System.out.println("Hello World");
+  System.out.println("MOunika");
 }
 }
